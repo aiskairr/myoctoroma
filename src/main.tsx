0d461e02345f6927436c6b04cbaf6b0c0e7c61e9
aktaevaktan <aktaevaktan@gmail.com>
2025-10-10 14:43:47 +0600

    fix dark theme for whole app

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,9 @@ import App from "./App";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
-import { ThemeProvider } from "./contexts/ThemeContext";
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <App />
   </QueryClientProvider>
 );
